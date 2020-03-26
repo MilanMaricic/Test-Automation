53e1e12a3564831aa43c46a8efceeac492beefbd
Milan Maricic <milanmgit@gmail.com>
2020-03-26 12:45:22 +0100

    Initialize Project5 by adding lib

---
+++ Project5/src/test/Main.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		FirstSeleniumTest test = new FirstSeleniumTest();
+		
+		test.setBrowser();
+		test.setBrowserConfig();
+		test.runTest();
+		
+	}
+
+}
